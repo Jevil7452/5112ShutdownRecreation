6dfb38d8493bfa71ee439eec2033b20d36779a85
Jevil7452 <98893064+Jevil7452@users.noreply.github.com>
2023-05-01 22:13:00 +0200

    better sizing? not sure if that's 1:1 to 5112 though

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,10 +30,10 @@
         {
             this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
@@ -45,15 +45,6 @@
             this.label1.Size = new System.Drawing.Size(198, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Windows will shut down in 30 seconds...";
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Image = global::_5112ShutdownRecreation.Properties.Resources.flag;
-            this.pictureBox2.Location = new System.Drawing.Point(39, 34);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(48, 48);
-            this.pictureBox2.TabIndex = 2;
-            this.pictureBox2.TabStop = false;
             // 
             // button1
             // 
@@ -80,12 +71,21 @@
             this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::_5112ShutdownRecreation.Properties.Resources.flag;
+            this.pictureBox2.Location = new System.Drawing.Point(39, 34);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(48, 48);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(393, 148);
+            this.ClientSize = new System.Drawing.Size(394, 149);
             this.ControlBox = false;
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -94,9 +94,9 @@
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(407, 182);
+            this.MaximumSize = new System.Drawing.Size(400, 175);
             this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(407, 182);
+            this.MinimumSize = new System.Drawing.Size(400, 175);
             this.Name = "Form1";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
